6a9bfac686ff5bae53c97c80f6fe17b83eae22bd
MBSciTech <maharshibhattisro@gmail.com>
2025-09-29 13:03:37 +0530

    Twoway textbox

---
+++ WpfApp_TwoWay/WpfApp_TwoWay/viewModel/viewModel1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp_TwoWay.viewModel
+{
+    internal class viewModel1 : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
