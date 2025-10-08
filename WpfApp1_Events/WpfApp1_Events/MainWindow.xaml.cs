856c5267c8da59dc12375949ce3863ae62b6c6ec
MBSciTech <maharshibhattisro@gmail.com>
2025-10-08 18:06:26 +0530

    Add WPF projects for Events and Calculator apps
    Added `WpfApp1_Events` solution with a WPF project targeting .NET 8.0.
    Implemented `App.xaml`, `MainWindow.xaml`, and event handlers for
    various UI controls like `TextBlock`, `Button`, `Slider`, and more.
    
    Added `WpfApp_Calculator` solution with a WPF project targeting .NET
    8.0. Created a calculator UI with a `Label` display and `Grid` layout
    for buttons. Implemented event handlers for arithmetic operations and
    display updates using `DataTable.Compute`.
    
    Included `AssemblyInfo.cs` in both projects for theme resource
    configuration. Configured project files with WPF settings and nullable
    reference types.

---
+++ WpfApp1_Events/WpfApp1_Events/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1_Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtblock_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txtblock.Text = "Haish mouse leaved";
+        }
+
+        private void txtblock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtblock.Text = "Ouch mouse entered";
+        }
+
+        private void lblname_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            lblname.Content = "Label Clicked!";
+        }
+
+        private void txtbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtbox.Background = System.Windows.Media.Brushes.LightBlue;
+        }
+
+        private void txtbox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtbox.Background = Brushes.LightCyan;
+        }
+
+        private void pwdInput_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Title = "Password length is " + pwdInput.Password.Length;
+        }
+
+        private void pwdInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            pwdInput.Background = Brushes.LightPink;
+        }
+
+        private void pwdInput_LostFocus(object sender, RoutedEventArgs e)
+        {
+            pwdInput.Background = Brushes.White;
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Clicked");
+        }
+
+        private void txtbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblname.Content = txtbox.Text;
+        }
+
+        private void txtbox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+                MessageBox.Show("Digits are not allowed");
+            }
+        }
+
+        private void btnSubmit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnSubmit.Background = Brushes.LightPink;
+            btnSubmit.Content = "Hovering";
+        }
+
+        private void btnSubmit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnSubmit.Background = Brushes.Green;
+            btnSubmit.Content = "Submit";
+        }
+
+        private void rbOption1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Title = "Radio button unchecked";
+        }
+
+        private void rbOption2_Checked(object sender, RoutedEventArgs e)
+        {
+            Title = ((RadioButton)sender).Content + "Selected!";
+
+        }
+
+        private void rbOption1_Checked(object sender, RoutedEventArgs e)
+        {
+            Title = ((RadioButton)sender).Content + "Selected!";
+        }
+
+        private void rbOption2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Title = "Radio button unchecked";
+        }
+
+        private void chkAccept_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Turms are accepted");
+        }
+
+        private void chkAccept_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Turms are not accepted");
+        }
+
+        
+
+        private void chkAccept_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Indeterminate state");
+        }
+
+        private void imgLogo_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lblname.Content = "Hovering of Maharshi's Image Ee hat!";
+        }
+
+        private void imgLogo_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Maharshi ni image click thai gai chhe!");
+        }
+
+        private void sldVolumn_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblname.Content = "Volume " + sldVolumn.Value.ToString("0");
+        }
+
+        private void sldVolumn_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            lblname.Content = "Slider draging started";
+        }
+
+        private void sldVolumn_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            lblname.Content = "Slider draging Completed";
+        }
+
+        private void cldEvent_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lblname.Content = "Selected Date : " + cldEvent.SelectedDate?.ToShortDateString();
+        }
+
+        private void cldEvent_DisplayDateChanged(object sender, CalendarDateChangedEventArgs e)
+        {
+            MessageBox.Show("calendar Mounth changed");
+        }
+
+        private void dpSelectedDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lblname.Content = "Pciked Date is " + dpSelectedDate.SelectedDate?.ToShortDateString();
+        }
+
+        private void dpSelectedDate_CalendarOpened(object sender, RoutedEventArgs e)
+        {
+            lblname.Content = "Date picker Opened!";
+        }
+
+        private void dpSelectedDate_CalendarClosed(object sender, RoutedEventArgs e)
+        {
+            lblname.Content = "Date picker Closed!";
+
+        }
+    }
+}
---
+++ WpfApp_Calculator/WpfApp_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AC_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = "";
+        }
+
+        private void modulo_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Convert.ToDecimal(Display.Content)/100;
+
+        }
+
+        private void plusminus_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Convert.ToInt32(Display.Content) * (-1);
+
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "7";
+
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "8";
+
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "9";
+
+        }
+
+        private void mul_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "*";
+
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "4";
+
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "5";
+
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "6";
+
+        }
+
+        private void min_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "-";
+
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "3";
+
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "2";
+
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "1";
+
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "+";
+
+        }
+
+        private void dot_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + ".";
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "0";
+        }
+
+        private void eqa_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = new DataTable().Compute(Display.Content.ToString(), "");
+
+        }
+
+        private void div_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = Display.Content + "/";
+
+        }
+    }
+}
