7e16a709b210fc27e6e9546c84cb6c1b895e6ac7
MBSciTech <maharshibhattisro@gmail.com>
2025-10-08 19:48:27 +0530

    Uploading WpfApp_FramePegging

---
+++ WpfApp_FramePegging/WpfApp_FramePegging/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_FramePegging
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void p1_Click(object sender, RoutedEventArgs e) {
+            MainFrame.Navigate(new Page3());
+        }
+        
+
+        private void p2_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page4());
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoForward)
+            {
+                MainFrame.GoForward();
+            }
+        }
+    }
+}
